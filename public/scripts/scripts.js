8445b2a1873a2c3f9d42074993308e21be7c6f53
Joel-Green <joelgreen737@gmail.com>
2019-07-04 16:24:22 +0530

    more basic stuff

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,36 @@
+// let onHeader = false;
+// $('.header').hover(function () {
+//     onHeader = true
+//     console.log('onheader', onHeader)
+// },
+//     function () {
+//         onHeader = false
+//         console.log('onheader', onHeader)
+//     }
+// )
+// $('#link-home').hover(function () {
+//     $('#nav-Home').toggleClass('show-dropdown')
+//     $('#nav-About').removeClass('show-dropdown')
+//     $('#nav-Outreach').removeClass('show-dropdown')
+//     $('#nav-Events').removeClass('show-dropdown')
+//     $('#nav-People').removeClass('show-dropdown')
+//     $('#nav-More').removeClass('show-dropdown')
+// })
+// $('#link-about').hover(function () {
+//     toggleAbout()
+// })
+// $('#link-outreach').hover(function () {
+//     toggleOutreach()
+// })
+// $('#link-events').hover(function () {
+//     toggleEvents()
+// })
+// $('#link-people').hover(function () {
+//     togglePeople()
+// })
+// $('#link-more').hover(function () {
+//     toggleMore()
+// })
 function toggleHome() {
     console.log('testings')
     $('#nav-Home').toggleClass('show-dropdown')
