5f5fa41a467daae03053b4ec9296737546866847
Joel-Green <joelgreen737@gmail.com>
2019-07-11 02:01:50 +0530

    +header scroll effect +fix content scroll effect + header sticky

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -251,28 +251,108 @@ content.forEach(section => {
 })
 const holdheading = document.querySelectorAll(".hold-heading")
 const holdcontent = document.querySelectorAll(".hold-content")
+const thresholdArray = steps => Array(steps + 1)
+    .fill(0)
+    .map((_, index) => index / steps || 0)
+
+let previousY = 0
+let previousRatio = 0
 console.log(holdheading);
 const options = {
     root: null,
-    threshold: 0,
-    rootMargin: "-40% 0px",
+    threshold: thresholdArray(20),
+    rootMargin: "-30% 0px",
 }
 
 const observerHold = new IntersectionObserver(function (entries, observerHold) {
     entries.forEach(entry => {
         // console.log(entry.target, " ", entry.isIntersecting);
-        if (!entry.isIntersecting) {
+        const currentRatio = entry.intersectionRatio;
+        const currentY = entry.boundingClientRect.y
+        // const currentRatio = entry.intersectionRatio
+        const isIntersecting = entry.isIntersecting
+        console.log(currentRatio);
+        // if (!entry.isIntersecting) {
 
-            console.log("Off- screen")
-            // entry.target.classList.remove('off-screen')
-        }
-        else {
-            console.log("ON SCREEN CHANGE NOW")
-            entry.target.classList.toggle('scroll-trigger')
-            holdcontent[0].classList.toggle('scroll-trigger');
+        //     console.log("Off- screen")
+        //     // entry.target.classList.remove('off-screen')
+        // }
+        // else {
+        //     console.log("ON SCREEN CHANGE NOW")
+        // entry.target.classList.toggle('scroll-trigger')
+        //     // console.log(entry.target.nextElementSibling.childNodes);
+        //     entry.target.nextElementSibling.childNodes.forEach(function (element) {
+        //         console.log(element.className.indexOf("hold-content"));
+        //         console.log(element.className);
 
+        //     });
+        //     // console.log;
+
+        //     // entry.target.nextSibling.childNodes[1].toggle('scroll-trigger');
+        //     // holdcontent[0].classList.toggle('scroll-trigger');
+
+        // }
+        if (currentY < previousY) {
+            console.log("Scrolling down ")
+
+            entry.target.classList.add('scroll-trigger')
+
+            entry.target.childNodes.forEach(function (element) {
+                try {
+                    if (element.className.indexOf("header-trigger")) {
+                        document.querySelector('.header').classList.add('header-scrolled');
+                    }
+                } catch (error) {
+
+                }
+            })
+
+            // } else {
+            // console.log("Scrolling down leave")
+            // entry.target.classList.add('scroll-trigger')
+            // }
+            entry.target.nextElementSibling.childNodes.forEach(function (element) {
+                try {
+                    if (element.className.indexOf("hold")) {
+                        element.classList.add("scroll-trigger");
+                    }
+
+                } catch (error) {
+
+                }
+            })
+        } else if (currentY > previousY && isIntersecting) {
+            // if (currentRatio < previousRatio) {
+            entry.target.nextElementSibling.childNodes.forEach(function (element) {
+                try {
+                    if (element.className.indexOf("hold")) {
+                        element.classList.remove("scroll-trigger");
+                    }
+
+                } catch (error) {
+
+                }
+            })
+            console.log("Scrolling up leave")
+            entry.target.classList.remove('scroll-trigger')
+            entry.target.childNodes.forEach(function (element) {
+                try {
+                    if (element.className.indexOf("header-trigger")) {
+                        document.querySelector('.header').classList.remove('header-scrolled');
+                    }
+                } catch (error) {
+
+                }
+            })
+            // } else {
+            // console.log("Scrolling up enter");
+            // entry.target.classList.remove('scroll-trigger')
+            // }
         }
-    });
+
+        previousY = currentY
+        previousRatio = currentRatio
+    })
 }, options)
 
 holdheading.forEach(section => {
