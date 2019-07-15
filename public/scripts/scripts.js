093527d11b07412a8ed0c8518ea1ab149b97d1f5
Joel-Green <joelgreen737@gmail.com>
2019-07-16 03:22:34 +0530

    revamped the revamped front page

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -204,6 +204,17 @@ function toggleMore() {
     $('#nav-Events').removeClass('show-dropdown')
     $('#nav-People').removeClass('show-dropdown')
 }
+const thresholdArray = steps => Array(steps + 1)
+    .fill(0)
+    .map((_, index) => index / steps || 0);
+
+
+for(size=9; size<100 && size >= 9; size=size*4/3)
+{
+    console.log(size);
+}
+
+
 window.onclick = function (event) {
     // console.log(event.target);
     if (!event.target.matches('.nav-item')) {
@@ -219,207 +230,224 @@ window.onclick = function (event) {
         }
     }
 }
+
 // const content = $('.content-main');
 // console.log(content);
-const content = document.querySelectorAll(".content-main")
-const contentone = document.querySelector('#intro')
+// const content = document.querySelectorAll(".content-main")
+// const contentone = document.querySelector('#intro')
 // console.log(content);
-const optionsBasic = {
-    root: null,
-    threshold: 0,
-    rootMargin: "-300px 0px -300px 0px",
-}
+// const optionsBasic = {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-300px 0px -300px 0px",
+// }
 
-const observer = new IntersectionObserver(function (entries, observer) {
-    entries.forEach(entry => {
-        // console.log(entry.target, " ", entry.isIntersecting);
-        if (!entry.isIntersecting) {
+// const observer = new IntersectionObserver(function (entries, observer) {
+//     entries.forEach(entry => {
+//         // console.log(entry.target, " ", entry.isIntersecting);
+//         if (!entry.isIntersecting) {
 
-            // console.log("np")
-            entry.target.classList.add('off-screen')
-        }
-        else {
-            // console.log("yeas")
-            entry.target.classList.remove('off-screen')
+//             // console.log("np")
+//             entry.target.classList.add('off-screen')
+//         }
+//         else {
+//             // console.log("yeas")
+//             entry.target.classList.remove('off-screen')
 
-        }
-    });
-}, optionsBasic)
+//         }
+//     });
+// }, optionsBasic)
 
-content.forEach(section => {
-    observer.observe(section);
+// content.forEach(section => {
+//     observer.observe(section);
 
-})
-const holdheading = document.querySelectorAll(".hold-heading")
-const holdcontent = document.querySelectorAll(".hold-content")
-const thresholdArray = steps => Array(steps + 1)
-    .fill(0)
-    .map((_, index) => index / steps || 0)
+// })
+// const holdheading = document.querySelectorAll(".hold-heading")
+// const holdcontent = document.querySelectorAll(".hold-content")
 
-let previousY = 0
-let previousRatio = 0
-// console.log(holdheading);
-const options = {
-    root: null,
-    threshold: thresholdArray(20),
-    rootMargin: "-45% 0px",
-}
+// let previousY = 0
+// let previousRatio = 0
+// // console.log(holdheading);
+// const options = {
+//     root: null,
+//     threshold: thresholdArray(20),
+//     rootMargin: "-45% 0px",
+// }
 
-const observerHold = new IntersectionObserver(function (entries, observerHold) {
-    entries.forEach(entry => {
-        // console.log(entry.target, " ", entry.isIntersecting);
-        const currentRatio = entry.intersectionRatio;
-        const currentY = entry.boundingClientRect.y
-        // const currentRatio = entry.intersectionRatio
-        const isIntersecting = entry.isIntersecting
-        // console.log(currentRatio);
-        // if (!entry.isIntersecting) {
+// const observerHold = new IntersectionObserver(function (entries, observerHold) {
+//     entries.forEach(entry => {
+//         // console.log(entry.target, " ", entry.isIntersecting);
+//         const currentRatio = entry.intersectionRatio;
+//         const currentY = entry.boundingClientRect.y
+//         // const currentRatio = entry.intersectionRatio
+//         const isIntersecting = entry.isIntersecting
+//         // console.log(currentRatio);
+//         // if (!entry.isIntersecting) {
 
-        //     console.log("Off- screen")
-        //     // entry.target.classList.remove('off-screen')
-        // }
-        // else {
-        //     console.log("ON SCREEN CHANGE NOW")
-        // entry.target.classList.toggle('scroll-trigger')
-        //     // console.log(entry.target.nextElementSibling.childNodes);
-        //     entry.target.nextElementSibling.childNodes.forEach(function (element) {
-        //         console.log(element.className.indexOf("hold-content"));
-        //         console.log(element.className);
+//         //     console.log("Off- screen")
+//         //     // entry.target.classList.remove('off-screen')
+//         // }
+//         // else {
+//         //     console.log("ON SCREEN CHANGE NOW")
+//         // entry.target.classList.toggle('scroll-trigger')
+//         //     // console.log(entry.target.nextElementSibling.childNodes);
+//         //     entry.target.nextElementSibling.childNodes.forEach(function (element) {
+//         //         console.log(element.className.indexOf("hold-content"));
+//         //         console.log(element.className);
 
-        //     });
-        //     // console.log;
+//         //     });
+//         //     // console.log;
 
-        //     // entry.target.nextSibling.childNodes[1].toggle('scroll-trigger');
-        //     // holdcontent[0].classList.toggle('scroll-trigger');
+//         //     // entry.target.nextSibling.childNodes[1].toggle('scroll-trigger');
+//         //     // holdcontent[0].classList.toggle('scroll-trigger');
 
-        // }
-        if (currentY < previousY) {
-            console.log("Scrolling down ")
+//         // }
+//         if (currentY < previousY) {
+//             console.log("Scrolling down ")
 
-            entry.target.classList.add('scroll-trigger')
+//             entry.target.classList.add('scroll-trigger')
 
-            entry.target.childNodes.forEach(function (element) {
-                try {
-                    if (element.className.indexOf("header-trigger")) {
-                        document.querySelector('.header').classList.add('header-scrolled');
-                    }
-                } catch (error) {
+//             entry.target.childNodes.forEach(function (element) {
+//                 try {
+//                     if (element.className.indexOf("header-trigger")) {
+//                         document.querySelector('.header').classList.add('header-scrolled');
+//                     }
+//                 } catch (error) {
 
-                }
-            })
+//                 }
+//             })
 
-            // } else {
-            // console.log("Scrolling down leave")
-            // entry.target.classList.add('scroll-trigger')
-            // }
-            entry.target.nextElementSibling.childNodes.forEach(function (element) {
-                try {
-                    if (element.className.indexOf("hold")) {
-                        element.classList.add("scroll-trigger");
-                    }
+//             // } else {
+//             // console.log("Scrolling down leave")
+//             // entry.target.classList.add('scroll-trigger')
+//             // }
+//             entry.target.nextElementSibling.childNodes.forEach(function (element) {
+//                 try {
+//                     if (element.className.indexOf("hold")) {
+//                         element.classList.add("scroll-trigger");
+//                     }
 
-                } catch (error) {
+//                 } catch (error) {
 
-                }
-            })
-        } else if (currentY > previousY && isIntersecting) {
-            // if (currentRatio < previousRatio) {
-            entry.target.nextElementSibling.childNodes.forEach(function (element) {
-                try {
-                    if (element.className.indexOf("hold")) {
-                        element.classList.remove("scroll-trigger");
-                    }
+//                 }
+//             })
+//         } else if (currentY > previousY && isIntersecting) {
+//             // if (currentRatio < previousRatio) {
+//             entry.target.nextElementSibling.childNodes.forEach(function (element) {
+//                 try {
+//                     if (element.className.indexOf("hold")) {
+//                         element.classList.remove("scroll-trigger");
+//                     }
 
-                } catch (error) {
+//                 } catch (error) {
 
-                }
-            })
-            console.log("Scrolling up leave")
-            entry.target.classList.remove('scroll-trigger')
-            entry.target.childNodes.forEach(function (element) {
-                try {
-                    if (element.className.indexOf("header-trigger")) {
-                        document.querySelector('.header').classList.remove('header-scrolled');
-                    }
-                } catch (error) {
+//                 }
+//             })
+//             console.log("Scrolling up leave")
+//             entry.target.classList.remove('scroll-trigger')
+//             entry.target.childNodes.forEach(function (element) {
+//                 try {
+//                     if (element.className.indexOf("header-trigger")) {
+//                         document.querySelector('.header').classList.remove('header-scrolled');
+//                     }
+//                 } catch (error) {
 
-                }
-            })
-            // } else {
-            // console.log("Scrolling up enter");
-            // entry.target.classList.remove('scroll-trigger')
-            // }
-        }
+//                 }
+//             })
+//             // } else {
+//             // console.log("Scrolling up enter");
+//             // entry.target.classList.remove('scroll-trigger')
+//             // }
+//         }
 
-        previousY = currentY
-        previousRatio = currentRatio
-    })
-}, options)
+//         previousY = currentY
+//         previousRatio = currentRatio
+//     })
+// }, options)
 
-holdheading.forEach(section => {
-    observerHold.observe(section);
+// holdheading.forEach(section => {
+//     observerHold.observe(section);
 
-})
+// })
 
-const  trigger = document.querySelectorAll('.trigger');
-const handleIntersect = entries => {
-    entries.forEach(entry => {
-        const currentY = entry.boundingClientRect.y
-        const currentRatio = entry.intersectionRatio
-        const isIntersecting = entry.isIntersecting
+// const  trigger = document.querySelectorAll('.trigger');
+// const handleIntersect = entries => {
+//     entries.forEach(entry => {
+//         const currentY = entry.boundingClientRect.y
+//         const currentRatio = entry.intersectionRatio
+//         const isIntersecting = entry.isIntersecting
 
-        // Scrolling down/up
-        console.log(currentRatio);
-        if (currentY < previousY) {
-            if (currentRatio > previousRatio && isIntersecting) {
-                console.log( "Scrolling down enter")
-            } else {
-                console.log( "Scrolling down leave")
-            }
-        } else if (currentY > previousY && isIntersecting) {
-            if (currentRatio < previousRatio) {
-                console.log( "Scrolling up leave")
-            } else {
-                console.log( "Scrolling up enter");
-            }
-        }
+//         // Scrolling down/up
+//         console.log(currentRatio);
+//         if (currentY < previousY) {
+//             if (currentRatio > previousRatio && isIntersecting) {
+//                 console.log( "Scrolling down enter")
+//             } else {
+//                 console.log( "Scrolling down leave")
+//             }
+//         } else if (currentY > previousY && isIntersecting) {
+//             if (currentRatio < previousRatio) {
+//                 console.log( "Scrolling up leave")
+//             } else {
+//                 console.log( "Scrolling up enter");
+//             }
+//         }
 
-        previousY = currentY
-        previousRatio = currentRatio
-    })
-}
+//         previousY = currentY
+//         previousRatio = currentRatio
+//     })
+// }
 
-const triggerObserve = new IntersectionObserver(handleIntersect, {
-    threshold: thresholdArray(20),
-    rootMargin: "-45% 0px",
-})
+// const triggerObserve = new IntersectionObserver(handleIntersect, {
+//     threshold: thresholdArray(20),
+//     rootMargin: "-45% 0px",
+// })
 
 
-console.log($('#intro'));
+// // console.log($('#intro'));
 
-const intro = document.querySelector('#intro');
-console.log(intro);
+// // const intro = document.querySelector('#intro');
+// // console.log(intro);
 
-optionIntro = {
+// // optionIntro = {
+// //     root:null,
+// //     threshold: thresholdArray(20), 
+// //     rootMargin : "0px"
+// // }
+// // const observeIntro = new IntersectionObserver ( function(entry, observeIntro){
+// //     // console.log(entry.isIntersecting);
+// //     console.log("Please Work");
+// //     // console.log(entry[0].intersectionRatio);
+// //     if(entry[0].intersectionRatio<0.998){
+// //         // console.log(entry[0].isIntersecting);
+// //         console.log("Please Work remove it");
+// //         document.getElementById("header").classList.remove('opacity-0');
+// //     }
+// //     else{
+// //         // console.log(entry[0].isIntersecting);
+// //         console.log("Please Work add it");
+// //         document.getElementById("header").classList.add('opacity-0');
+// //     }
+
+// // }, optionIntro)
+
+// // observeIntro.observe(intro);
+
+let homeHeader =  document.querySelectorAll(".homeHeaderTrigger");
+let introHeader = document.querySelectorAll('.home-header');
+let introContent = document.getElementById('intro');
+console.log(homeHeader);
+
+optionsHomeHeader = {
     root:null,
-    threshold: thresholdArray(20), 
-    rootMargin : "0px"
+    threshold :thresholdArray(30),
+    rootMargin : "0px 0px 0px 0px"
 }
-const observeIntro = new IntersectionObserver ( function(entry, observeIntro){
-    // console.log(entry.isIntersecting);
-    console.log("Please Work");
+const observeHomeHeader = new IntersectionObserver (function(entry,observeHomeHeader){
     // console.log(entry[0].intersectionRatio);
-    if(entry[0].intersectionRatio<0.998){
-        // console.log(entry[0].isIntersecting);
-        console.log("Please Work remove it");
-        document.getElementById("header").classList.remove('opacity-0');
-    }
-    else{
-        // console.log(entry[0].isIntersecting);
-        console.log("Please Work add it");
-        document.getElementById("header").classList.add('opacity-0');
-    }
+    introHeader[0].style.opacity=3*entry[0].intersectionRatio-1.64;
+    introContent.style.opacity=3*entry[0].intersectionRatio-1.64;
+    console.log(3*entry[0].intersectionRatio-1.94);
+},optionsHomeHeader);
 
-}, optionIntro)
-
-observeIntro.observe(intro);
+observeHomeHeader.observe(homeHeader[0]);
